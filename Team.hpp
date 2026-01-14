e20d97f640d32c13fd493716824b84315c99380b
erantala <erantala@student.hive.fi>
2026-01-14 16:42:20 +0200

    Documentation, and refactoring of header file

--- Team.hpp
+++ Team.hpp
@@ -62,7 +62,7 @@ enum	heroes
 	TRACER,
 	VENTURE,
 	WIDOW,
-    VENDETTA,
+	VENDETTA,
 	ANA,
 	BAP,
 	BRIG,
@@ -78,20 +78,20 @@ enum	heroes
 };
 
 const 	vector<string>	_heroes = { "DVA", "DOOM", "HAZ", "JQ", "MAUGA", "ORI", "RAM", "REIN",
-                                "HOG", "SIG", "WINSTON", "BALL", "ZARYA", "ASHE", "BASTION", "CASS", "ECHO",
-                                "FREJA", "GENJI", "HANZO", "JUNK", "MEI", "PHARAH", "REAPER", "SOJ", "SOLDIER",
-                                "SOMBRA", "SYM", "TORB", "TRACER", "VENTURE", "VENDETTA", "WIDOW", "ANA", "BAP",
-                                "BRIG", "ILLARI", "JUNO", "KIRI", "LW", "LUCIO", "MERCY", "MOIRA", "WU", "ZEN"};
+	"HOG", "SIG", "WINSTON", "BALL", "ZARYA", "ASHE", "BASTION", "CASS", "ECHO",
+	"FREJA", "GENJI", "HANZO", "JUNK", "MEI", "PHARAH", "REAPER", "SOJ", "SOLDIER",
+	"SOMBRA", "SYM", "TORB", "TRACER", "VENTURE", "VENDETTA", "WIDOW", "ANA", "BAP",
+	"BRIG", "ILLARI", "JUNO", "KIRI", "LW", "LUCIO", "MERCY", "MOIRA", "WU", "ZEN"};
 const	vector<string>	_maps = { "ANTARCTIC - ICEBREAKER", "ANTARCTIC - LABS", "ANTARCTIC - SUBLEVEL",
-                              "BUSAN - DOWNTOWN", "BUSAN - SANCTUARY", "BUSAN - MEKABASE",
-                              "ILIOS - LIGHTHOUSE", "ILIOS - WELL", "ILIOS - RUINS",
-                              "LIJANG - NIGHTMARKET", "LIJANG - GARDENS", "LIJANG - CONTROL CENTER"
-                              "NEPAL - VILLAGE", "NEPAL - SHRINE", "NEPAL - SANCTUM",
-                              "OASIS - CITY CENTER", "OASIS - GARDENS", "OASIS - UNIVERSITY",
-                              "SAMOA - BEACH", "SAMOA - DOWNTOWN", "SAMOA - VOLCANO", "CIRCUIT",
-                              "DORADO", "HAVANA", "JUNKERTOWN", "RIALTO", "ROUTE",
-                              "SHAMBALI", "GIB", "AATLIS", "NEW JUNK CITY", "SURAVASA", "BLIZZ", "EICH", "HOLLYWOOD",  "KR",
-                              "MIDTOWN", "NUMBANI", "PARAISO", "COLOSSEO", "ESPERANCA", "NQS", "RUNASAPI" };
+	"BUSAN - DOWNTOWN", "BUSAN - SANCTUARY", "BUSAN - MEKABASE",
+	"ILIOS - LIGHTHOUSE", "ILIOS - WELL", "ILIOS - RUINS",
+	"LIJANG - NIGHTMARKET", "LIJANG - GARDENS", "LIJANG - CONTROL CENTER"
+	"NEPAL - VILLAGE", "NEPAL - SHRINE", "NEPAL - SANCTUM",
+	"OASIS - CITY CENTER", "OASIS - GARDENS", "OASIS - UNIVERSITY",
+	"SAMOA - BEACH", "SAMOA - DOWNTOWN", "SAMOA - VOLCANO", "CIRCUIT",
+	"DORADO", "HAVANA", "JUNKERTOWN", "RIALTO", "ROUTE",
+	"SHAMBALI", "GIB", "AATLIS", "NEW JUNK CITY", "SURAVASA", "BLIZZ", "EICH", "HOLLYWOOD",  "KR",
+	"MIDTOWN", "NUMBANI", "PARAISO", "COLOSSEO", "ESPERANCA", "NQS", "RUNASAPI" };
 
 #define MAPAMT _maps.size()
 
@@ -117,21 +117,34 @@ class Team
 		Team(string name);
 		~Team();
 		void	getStats();
-        size_t     getCompAmt();
-        string	retStats();
-        QString retQstats(QTextEdit *info);
+		size_t	 getCompAmt();
+		string	retStats();
+		/*
+			Applies a teams stats info to QTextEdit in view stats page
+			@param pointer to the QTextEdit box
+		*/
+		QString retQstats(QTextEdit *info);
 		void	resetStats();
 		void	getHeroes();
 		void	setNotes(string notes);
 		void	addHeroes(size_t i);
 		void	newComp();
-        void	addComp(string heroes[6]);
-        void	addComp(int heroes[6]);
-        void    changeComp(int heroes[5], int index);
+		void	addComp(string heroes[6]);
+		/*
+			Adds a full team composition based on indexes
+			@param array of 6 indexes corresponding to TANK, DPS, DPS, SUPP, SUPP, MAP
+		*/
+		void	addComp(int heroes[6]);
+		/*
+			Adjusts a previously made team composition
+			@param Array of new hero indexes for all roles
+			@param Index of comp inside class
+		*/
+		void	changeComp(int heroes[5], int index);
 		void	deleteTeam();
 		void	updateTime();
 		void	printComp(size_t i);
-        string    returnComp(size_t i);
+		string  returnComp(size_t i);
 		void	printAllComps();
 		void	printTanks();
 		void	printSupps();
@@ -140,10 +153,10 @@ class Team
 		void	saveTeam();
 		void	selectMap(int i);
 		void	displayMapStats();
-        string  retMapStats(int map);
+		string  retMapStats(int map);
 		vector<map<string, string>> getComps() const;
 		string	getName() const;
-        string  *getComp(size_t i);
+		string  *getComp(size_t i);
 		string	getSaveName() const;
 		time_t	getCreated() const;
 		time_t	getUpdated() const;
